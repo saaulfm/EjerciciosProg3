a1a7ae7dd6f260eec35929c82f3b0c4ec0a6827b
Saul Fernandez <saul.f.m@opendeusto.es>
2024-11-09 11:27:05 +0100

    Clase JFrameLanzamientos
    Clase que define la ventana principal de la interfaz gráfica para
    visualizar lanzamientos de cohetes utilizando un JTree

---
+++ Tema3_Interfaces_Ejercicio4/src/JFrameLanzamientos.java
@@ -0,0 +1,28 @@
+
+import java.awt.BorderLayout;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTree;
+import javax.swing.border.TitledBorder;
+
+public class JFrameLanzamientos extends JFrame {
+	private static final long serialVersionUID = 1L;
+	
+	public JFrameLanzamientos(Map<String, List<RocketLaunch>> launches) {
+		JTree jTreeCompanies = new JTree();		
+		JScrollPane scrollPane = new JScrollPane(jTreeCompanies);
+		scrollPane.setBorder(new TitledBorder("Rocket Launches"));
+		
+		this.setTitle("Rocket Launches GUI");
+		this.setLayout(new BorderLayout(0, 0));		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.add(scrollPane, BorderLayout.CENTER);
+
+		this.setSize(800, 600);
+		this.setLocationRelativeTo(null);		
+		this.setVisible(true);		
+	}	
+}
